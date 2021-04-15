3c868052704e5bda7bceb4991b113d3ce3c4b837
shaanaye <72814715+shaanaye@users.noreply.github.com>
2021-04-14 22:30:59 -0400

    Fixed monitoring system so it doesn't repeatedly send webhook. Improved webhooks that are sent to include more information Fixed View Counters

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from dhooks import Webhook, Embed
 import threading
 import time
 
+
 client = commands.Bot(command_prefix='.')
 
 main_class = ""
@@ -18,6 +19,8 @@ ebay_listing = ""
 listing_image = ""
 sent_webhooks = []
 count = 0
+listing_name = ""
+price = ""
 
 
 
@@ -42,14 +45,14 @@ async def setup(ctx):
 
         return check_msg
 
-    ebay_username = await client.wait_for("message", check=wrapper(ctx))
-
+    user = await client.wait_for("message", check=wrapper(ctx))
+    ebay_username = user.content
     second_embed = discord.Embed(title="ViewBot V2", color=0x00ffee)
-    second_embed.add_field(name=f"Now Monitoring {ebay_username.content}'s Account", value="---------", inline=True)
+    second_embed.add_field(name=f"Now Monitoring {user.content}'s Account", value="---------", inline=True)
     second_embed.set_footer(text="Created by shaan")
     await ctx.send(embed=second_embed)
 
-    ebay_accounts.append(ebay_username.content)
+    ebay_accounts.append(user.content)
 
 
 @client.command()
@@ -58,12 +61,10 @@ async def start(ctx):
     global listing_image
     global sent_webhooks
     global count
+    global listing_name
+    global price
+    global ebay_username
     print("starting")
-    print(ebay_listing)
-
-
-
-
 
     def initial_request():
         global sent_webhooks
@@ -72,7 +73,8 @@ async def start(ctx):
         global listing_image
         global ebay_listing
         global new_listings
-        r = requests.get("https://www.ebay.com/sch/aaarated7/m.html?_nkw=&_armrs=1&_ipg=&_from=")
+        global ebay_username
+        r = requests.get("https://www.ebay.com/sch/"+str(ebay_username)+"/m.html?_nkw=&_armrs=1&_ipg=&_from=")
         print("sent request")
         soup = BeautifulSoup(r.content, "html.parser")
         main_class = soup.find_all(class_="lvtitle")
@@ -84,7 +86,7 @@ async def start(ctx):
             if "New Listing".lower() in str(item).lower():
                 new_listings.append(item)
             else:
-                print("No New Listings")
+                pass
         listings.clear()
         print(new_listings)
 
@@ -116,23 +118,39 @@ async def start(ctx):
         image_parse2 = image_parse[1].split('"')
         listing_image = image_parse2[0]
 
-    def find_url():
+    def find_url(sent_listing):
         global ebay_listing
         parse = str(sent_listing)
         parse1 = parse.split('href="')
         parse2 = parse1[1].split('"')
         ebay_listing = str(parse2[0])
 
+    def listing_information():
+        global ebay_listing
+        global listing_name
+        global price
+        r = requests.get(ebay_listing)
+        soup = BeautifulSoup(r.content, "html.parser")
+        listing_name1 = soup.find(id="itemTitle")
+        listing_name2 = str(listing_name1).split("</span>")
+        listing_name3 = str(listing_name2[1]).split("</h1>")
+        listing_name = listing_name3[0]
+        price1 = soup.find(id="prcIsum")
+        price2 = str(price1).split('content="')
+        price3 = str(price2[1]).split('" id')
+        price = str(price3[0])
+
+
     threads = []
 
     def viewer():
         count = 0
-        while count < 5:
+        while count < 300:
             response = requests.get(ebay_listing)
             count += 1
 
     def start_threads():
-        for _ in range(2):
+        for _ in range(20):
             t = threading.Thread(target=viewer)
             t.start()
             threads.append(t)
@@ -142,19 +160,43 @@ async def start(ctx):
 
 
     while True:
+        initial_sent_webhook_length = len(sent_webhooks)
         initial_request()
         if count == 0:
             for sent_listing in sent_webhooks:
-                find_url()
+                find_url(sent_listing)
                 find_image()
+                listing_information()
                 new_listing_embed = Embed(
                     description=f'[**New Listing Found!**]({ebay_listing})',
                     color=0x00FF00,
                 )
-                new_listing_embed.add_field(name="Added Views", value="-----------------")
+                new_listing_embed.add_field(name="Added Views to:", value=f"{listing_name}", inline=False)
+                new_listing_embed.add_field(name="User:", value=f'{ebay_username}', inline=True)
+                new_listing_embed.add_field(name="Price:", value=f'{price}', inline=True)
+                new_listing_embed.set_image(url=listing_image)
+                new_listing_embed.set_footer(text="Created by shaan")
                 await ctx.send(embed=new_listing_embed)
-                print(ebay_listing)
-        time.sleep(20)
+            count += 1
+        else:
+            prior_sent_webhook_length = len(sent_webhooks)
+            if prior_sent_webhook_length > initial_sent_webhook_length:
+                find_url(sent_webhooks[-1])
+                find_image()
+                listing_information()
+                new_listing_embed = Embed(
+                    description=f'[**New Listing Found!**]({ebay_listing})',
+                    color=0x00FF00
+                )
+                new_listing_embed.add_field(name="Added Views to:", value=f"{listing_name}", inline=False)
+                new_listing_embed.add_field(name="User:", value=f'{ebay_username}', inline=True)
+                new_listing_embed.add_field(name="Price:", value=f'{price}', inline=True)
+                new_listing_embed.set_image(url=listing_image)
+                new_listing_embed.set_footer(text="Created by shaan")
+                await ctx.send(embed=new_listing_embed)
+            else:
+                pass
 
+        time.sleep(60)
 
 client.run("ODI3OTE1NzgzMDEwMjU0ODg4.YGh-qA.gUBmyAJGyb8u2X5taS6UCCTnN-k")
